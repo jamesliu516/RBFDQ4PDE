86255809bbf8230c58003b26920ff0043e7ede71
Jianming Liu <jmliu@jsnu.edu.cn>
2017-11-11 18:22:28 +0800

    obtain the support points around a point (not include the point)

--- generateCircle.m
+++ generateCircle.m
@@ -3,7 +3,7 @@
 %   Copyright (C) 2004-2012 Per-Olof Persson. See COPYRIGHT.TXT for details.
 clear 
 clc
-global p t
+global p t meshden
 rand('state',1); % Always the same results
 set(gcf,'rend','z');
 
@@ -12,8 +12,9 @@ fstats=@(p,t) fprintf('%d nodes, %d elements, min quality %.2f\n', ...
 
 fprintf('Uniform Mesh on Unit Circle\n');
 echo on
-fd=@(p) sqrt(sum(p.^2,2))-1;
-[p,t]=distmesh2d(fd,@huniform,0.1,[-1,-1;1,1],[]);
+ra=1;
+fd=@(p) sqrt(sum(p.^2,2))-ra;
+[p,t]=distmesh2d(fd,@huniform,meshden,[-ra,-ra;ra,ra],[]);
 echo off
 fstats(p,t);
 fprintf('(Done mesh generation.)\n\n')
--- meshfreeTreat.m
+++ meshfreeTreat.m
@@ -1,24 +1,39 @@
-generateCircle;
 
-global p t;
+clear all
+clf
+global p t meshden;
+meshden=0.04;
+
+generateCircle;
 
 nelem=size(t,1);
 npoin=size(p,1);
 
-pstart=zeros(npoin,1); 
-pend=zeros(npoin,1);
+%pstart=zeros(npoin,1);
+%pend=zeros(npoin,1);
 threeP=3;
-maxElem=20;
-elemPoint=zeros(npoin,maxElem); %elements aroung a point
+maxElems=20;
+maxPoints=25;
+elemPoint=zeros(npoin,maxElems); %elements aroung a point
+pointsPoint=zeros(npoin,maxPoints);
 
 n_elemPoint=zeros(npoin,1); %% the number of element with common point
-pointElem=[]; % point with elements
+%包含点的单元数目
+n_pointPoint=zeros(npoin,1); %% the number of point(support points) with common point
+%包含点的数目
+
+%pointElem=[]; % point with elements
+
 for iele=1:nelem
     ti=t(iele,:);
     for i=1:threeP
-        if n_elemPoint(ti(i))==0            
+        if n_elemPoint(ti(i))==0
             n_elemPoint(ti(i))=n_elemPoint(ti(i))+1;
-            elemPoint(ti(i),n_elemPoint(ti(i)))=iele; 
+            if n_elemPoint(ti(i)) > maxElems
+                disp('the number of element with common point more than maxElem');
+                return;
+            end
+            elemPoint(ti(i),n_elemPoint(ti(i)))=iele;
         else
             flag=0;
             for j=1:n_elemPoint(ti(i))
@@ -29,18 +44,39 @@ for iele=1:nelem
             end
             if flag==0
                 n_elemPoint(ti(i))=n_elemPoint(ti(i))+1;
+                if n_elemPoint(ti(i)) > maxElems
+                    disp('the number of element with common point more than maxElem');
+                    return;
+                end
                 elemPoint(ti(i),n_elemPoint(ti(i)))=iele;
             end
         end
     end
 end
-                
-                
-                
-                    
-                    
-            
 
+for ipoin=1:npoin
+    for ielem=1:n_elemPoint(ipoin)
+        ti=t(elemPoint(ipoin,ielem));
+        for i=1:threeP
+            if ti(i) ~= ipoin
+                if n_pointPoint(ipoin)==0
+                    n_pointPoint(ipoin)=n_pointPoint(ipoin)+1;
+                    pointsPoin(ipoin,n_pointPoint(ipoin))=ti(i);
+                else
+                    flag=0;
+                    for j=1:n_pointPoint(ipoin)
+                        if pointsPoin(ipoin,j)==ti(i)
+                            flag=1;
+                            break
+                        end
+                    end
+                   
+               
         
-    
-clear ptmp
+
+
+
+
+
+
+
